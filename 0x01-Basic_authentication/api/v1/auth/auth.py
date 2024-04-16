fc12945a93ba86f86139278f60108c1152fcf550
berekettena <tena21bereket@gmail.com>
2024-04-16 20:22:04 +0300

    Request validation!

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -31,6 +31,7 @@ class Auth:
         """
         if request is None:
             return None
+        return request.headers.get("Authorization")
 
     def current_user(self, request=None) -> TypeVar('User'):
         """
