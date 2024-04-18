4b7991c8f0bff620bce9e51f9a29f9ddf5999e9b
berekettena <tena21bereket@gmail.com>
2024-04-18 17:19:21 +0300

    Logout

--- 0x02-Session_authentication/api/v1/views/session_auth.py
+++ 0x02-Session_authentication/api/v1/views/session_auth.py
@@ -47,4 +47,4 @@ def logout() -> Tuple[str, int]:
     from api.v1.app import auth
     if not auth.destroy_session(request):
         abort(404)
-    return jsonify({}), 200
+    return jsonify({})
