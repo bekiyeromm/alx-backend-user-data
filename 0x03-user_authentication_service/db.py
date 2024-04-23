6c18cce59fe271b0817a09cc143877e9527475e1
berekettena <tena21bereket@gmail.com>
2024-04-23 22:13:28 +0300

    implement the DB.find_user_by method

---
+++ 0x03-user_authentication_service/db.py
@@ -0,0 +1,73 @@
+#!/usr/bin/env python3
+"""DB module.
+"""
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm.session import Session
+
+from user import Base, User
+from sqlalchemy.exc import NoResultFound, InvalidRequestError
+from sqlalchemy import tuple_
+
+
+class DB:
+    """DB class.
+    """
+
+    def __init__(self) -> None:
+        """Initialize a new DB instance.
+        """
+        self._engine = create_engine("sqlite:///a.db", echo=False)
+        Base.metadata.drop_all(self._engine)
+        Base.metadata.create_all(self._engine)
+        self.__session = None
+
+    @property
+    def _session(self) -> Session:
+        """Memoized session object.
+        """
+        if self.__session is None:
+            DBSession = sessionmaker(bind=self._engine)
+            self.__session = DBSession()
+        return self.__session
+
+    def add_user(self, email: str, hashed_password: str) -> User:
+        '''
+        Creates a new user for DB
+        '''
+        try:
+            new_user = User(email=email, hashed_password=hashed_password)
+            self._session.add(new_user)
+            self._session.commit()
+        except Exception:
+            self._session.rollback()
+            new_user = None
+        return new_user
+
+    def find_user_by(self, **kwargs) -> User:
+        """Find a user by filtering the users table with the given arguments
+
+        Args:
+            **kwargs: Arbitrary keyword arguments to filter the users table
+
+        Returns:
+            User: The first User object found matching the filter criteria
+
+        Raises:
+            NoResultFound: If no user is found matching the filter criteria
+            InvalidRequestError: If wrong query arguments are passed
+        """
+        fields, values = [], []
+        for key, value in kwargs.items():
+            if hasattr(User, key):
+                fields.append(getattr(User, key))
+                values.append(value)
+            else:
+                raise InvalidRequestError()
+        result = self._session.query(User).filter(
+            tuple_(*fields).in_([tuple(values)])
+        ).first()
+        if result is None:
+            raise NoResultFound()
+        return result
