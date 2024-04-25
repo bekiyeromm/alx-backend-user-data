bf5ed56d70068c2fc2ea3e4961deae959e0ab2b4
berekettena <tena21bereket@gmail.com>
2024-04-25 15:57:53 +0300

    login

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -3,7 +3,7 @@
 Basic Flask app
 """
 
-from flask import Flask, jsonify
+from flask import Flask, jsonify, make_response, abort
 from flask import request
 from auth import Auth
 
@@ -41,6 +41,21 @@ def register_user():
     except ValueError as e:
         """User with email already exists"""
         return jsonify({"message": "email already registered"}), 400
+
+
+@app.route('/sessions', methods=['POST'])
+def login():
+    """
+    accept email and password from form data
+    """
+    email = request.form.get('email')
+    password = request.form.get('password')
+    if not AUTH.valid_login(email, password):
+        abort(401)
+    session_id = AUTH.create_session(email)
+    resp = jsonify({"email": email, "message": "logged in"})
+    resp.set_cookie("session_id", session_id)
+    return resp
 
 
 if __name__ == "__main__":
